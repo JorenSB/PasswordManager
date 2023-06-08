9032d3d2f037242658659f5d9bc7a27eec2d0b91
Justwouter <wouter2312@gmail.com>
2023-06-08 15:03:45 +0200

    Added a JSON serializer for presistent data

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    static final PasswordList passwordList = new PasswordList(new FileWriter("data.txt"));
+    static final PasswordList passwordList = new PasswordList(new JsonWriter("data.json"));
     static final PasswordToolsFactory passwordToolsFactory = new SimplePasswordToolsFactory();
     public static void main(String[] args) {
         new MainMenu("Main Menu", passwordList, passwordToolsFactory).execute();
